d5e97e7c03f1e0c08703c4c5d7bb53ba52a96aa7
jaoluiz13 <luizcostajoao12@gmail.com>
2022-04-02 18:21:45 -0300

    feat: ajustes

--- public/main.js
+++ public/main.js
@@ -11,23 +11,35 @@ let textInput = document.querySelector("#chatTextInput");
 loginPage.style.display = "flex";
 chatPage.style.display = "none";
 
-function renderUserList (){
-    let ul = document.querySelector('.userList');
-    ul.innerHTML = '';
+function renderUserList() {
+  let ul = document.querySelector(".userList");
+  //ul.innerHTML = "";
 
-    userList.forEach(i=>{
-        ul.innerHTML += `<li>${i}</li>`
-    });
+  userList.forEach((i) => {
+    ul.innerHTML += `<li>${i}</li>`;
+  });
+}
+
+function addMessage(type, user, message) {
+  let ul = document.querySelector(".chatList");
+  switch (type) {
+    case "status":
+      ul.innerHTML += `<li class="m-status">${message}</li>`;
+      break;
+    case "message":
+      ul.innerHTML += `<li class="m-txt"><span>${user}: </span>${message}</li>`;
+      break;
+  }
 }
 
 loginInput.addEventListener("keyup", (e) => {
-  if (e.keyCode == 13) {
+  if (e.keyCode === 13) {
     let name = loginInput.value.trim();
     if (name != "") {
       userName = name;
       document.title = `Chat:${userName}`;
 
-      socket.emit('join-request', userName);
+      socket.emit("join-request", userName);
     } else {
       loginInput.style.border = "1px solid red";
       alert("Por favor digite seu nome");
@@ -35,11 +47,45 @@ loginInput.addEventListener("keyup", (e) => {
   }
 });
 
-socket.on("user-ok", (list) => {
-    loginPage.style.display = 'none';
-    chatPage.style.display = 'flex'
-    textInput.focus();
+textInput.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    let message = textInput.value.trim();
+    textInput.value = '';
+    if (message != '') {
+        socket.emit("send-msg", message);
+        
+      } else {
+        textInput.style.border = "1px solid red";
+        alert("Por favor digite uma mensagem!");
+      }
+ 
+  }
+  
+});
 
-    userList = list
-    renderUserList();
+socket.on("user-ok", (list) => {
+  loginPage.style.display = "none";
+  chatPage.style.display = "flex";
+  textInput.focus();
+
+  addMessage("status", null, "Conectado!");
+
+  userList = list;
+  renderUserList();
+});
+
+socket.on("list-update", (data) => {
+  if (data.joined) {
+    addMessage("status", null, `${data.joined} entrou no chat as ${data.time}`);
+  }
+
+  if (data.left) {
+    addMessage("status", null, `${data.left} saiu do chat  as ${data.time}`);
+  }
+  userList = data.list;
+  renderUserList();
+});
+
+socket.on("show-msg", (data) => {
+  addMessage("message", `${data.username}`, `${data.message}| ${data.time}`);
 });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const http = require("http");
 const socketIO = require("socket.io");
 const { Socket } = require("dgram");
+const { time } = require("console");
 
 const app = express();
 const server = http.createServer(app);
@@ -13,17 +14,46 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let connectedUSers = [];
 
-io.on('connection', (socket) => {
-    console.log('Conectado');
+io.on("connection", (socket) => {
+  console.log("Conectado");
+  const data = new Date();
+  socket.on("join-request", (username) => {
+    socket.username = username;
+    connectedUSers.push(username);
+    console.log(connectedUSers);
 
-    socket.on('join-request', (username) => {
-        socket.username = username;
-        connectedUSers.push(username);
-        console.log(connectedUSers);
+    socket.emit("user-ok", connectedUSers);
+    let time = "";
+    time = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
 
-        socket.emit('user-ok', connectedUSers);
-
-        
+    socket.broadcast.emit("list-update", {
+      joined: username,
+      list: connectedUSers,
+      time: time,
     });
+  });
 
+  socket.on("disconnect", () => {
+    connectedUSers = connectedUSers.filter((u) => u != socket.username);
+    console.log(connectedUSers);
+    let time = "";
+    time = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+    socket.broadcast.emit("list-update", {
+      left: socket.username,
+      list: connectedUSers,
+      time: time,
+    });
+  });
+
+  socket.on("send-msg", (msg) => {
+    let time = "";
+    time = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+    let obj = {
+      username: socket.username,
+      message: msg,
+      time: time,
+    };
+    socket.emit("show-msg", obj);
+    socket.broadcast.emit("show-msg", obj);
+  });
 });
