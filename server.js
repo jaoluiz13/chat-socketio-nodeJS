196845f86450cc6eb58dcd73ced829849bc3b3c6
jaoluiz13 <luizcostajoao12@gmail.com>
2022-04-02 18:44:51 -0300

    deploy

--- server.js
+++ server.js
@@ -4,61 +4,65 @@ const http = require("http");
 const socketIO = require("socket.io");
 const { Socket } = require("dgram");
 const { time } = require("console");
-import dotenv from 'dotenv';
-
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
+const router = express.Router();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+router.get("/", (req, res) => {
+  let connectedUSers = [];
+
+  io.on("connection", (socket) => {
+    console.log("Conectado");
+    const data = new Date();
+    socket.on("join-request", (username) => {
+      socket.username = username;
+      connectedUSers.push(username);
+      console.log(connectedUSers);
+
+      socket.emit("user-ok", connectedUSers);
+      let time = "";
+      time =
+        data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+
+      socket.broadcast.emit("list-update", {
+        joined: username,
+        list: connectedUSers,
+        time: time,
+      });
+    });
+
+    socket.on("disconnect", () => {
+      connectedUSers = connectedUSers.filter((u) => u != socket.username);
+      console.log(connectedUSers);
+      let time = "";
+      time =
+        data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+      socket.broadcast.emit("list-update", {
+        left: socket.username,
+        list: connectedUSers,
+        time: time,
+      });
+    });
+
+    socket.on("send-msg", (msg) => {
+      let time = "";
+      time =
+        data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+      let obj = {
+        username: socket.username,
+        message: msg,
+        time: time,
+      };
+      socket.emit("show-msg", obj);
+      socket.broadcast.emit("show-msg", obj);
+    });
+  });
+});
 
 server.listen(process.env.PORT);
 app.use(express.static(path.join(__dirname, "public")));
-
-let connectedUSers = [];
-
-io.on("connection", (socket) => {
-  console.log("Conectado");
-  const data = new Date();
-  socket.on("join-request", (username) => {
-    socket.username = username;
-    connectedUSers.push(username);
-    console.log(connectedUSers);
-
-    socket.emit("user-ok", connectedUSers);
-    let time = "";
-    time = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
-
-    socket.broadcast.emit("list-update", {
-      joined: username,
-      list: connectedUSers,
-      time: time,
-    });
-  });
-
-  socket.on("disconnect", () => {
-    connectedUSers = connectedUSers.filter((u) => u != socket.username);
-    console.log(connectedUSers);
-    let time = "";
-    time = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
-    socket.broadcast.emit("list-update", {
-      left: socket.username,
-      list: connectedUSers,
-      time: time,
-    });
-  });
-
-  socket.on("send-msg", (msg) => {
-    let time = "";
-    time = data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
-    let obj = {
-      username: socket.username,
-      message: msg,
-      time: time,
-    };
-    socket.emit("show-msg", obj);
-    socket.broadcast.emit("show-msg", obj);
-  });
-});
